acbae95a354a2a0991fa55d587a4b1c9dd3b5651
Nabeil <nabeillemar@gmail.com>
2021-04-10 23:44:28 -0700

    commit tree 6b166621cb302eb97e42c409ec342cc368311fe9

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+      end
+
+    get "/" do 
+        erb :index
+    end 
+
+    post '/checkout' do 
+        @item = params[:item]
+
+        session["item"] = "socks"
+        @session = session 
+
+        erb :output
+    end 
+
 end
